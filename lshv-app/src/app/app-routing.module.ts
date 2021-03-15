27425c23225b4ced86eb1ad5d85373afd6a2d240
Luis <luis.riverahuaman@hotmail.com>
2021-03-15 14:28:21 -0500

    vista

--- lshv-app/src/app/app-routing.module.ts
+++ lshv-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './component/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "Menu", component: MenuComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
